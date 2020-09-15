05b2d1792874acf7bbd82e123a8b7af38df36c65
edabrito7 <67392231+edabrito7@users.noreply.github.com>
2020-09-15 10:42:05 -0400

    Adding config files

---
+++ src/clarifai/clarifai.js
@@ -0,0 +1,12 @@
+import Clarifai from 'clarifai';
+
+
+
+
+
+const clarifaiApp = new Clarifai.App({
+    apiKey: '7296aef29a42412a96596564c671ce6f'
+})
+
+
+export default clarifaiApp;
---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5lUd0ZjE6EqRwKd8k2CMnbPWm422A1DQ",
+    authDomain: "what-ve-eaten.firebaseapp.com",
+    databaseURL: "https://what-ve-eaten.firebaseio.com",
+    projectId: "what-ve-eaten",
+    storageBucket: "what-ve-eaten.appspot.com",
+    messagingSenderId: "759838401269",
+    appId: "1:759838401269:web:5f5a222343a3cbfc7c1bbc"
+  };
+
+
+
+  export default firebaseConfig;
